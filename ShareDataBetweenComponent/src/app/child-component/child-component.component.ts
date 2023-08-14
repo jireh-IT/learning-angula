85c4b885370bfdae0296c3cd5bb75a39c53e16bb
jireh-IT <kapangajireh@gmail.com>
2023-08-14 17:46:03 +0200

    initial commit

---
+++ ShareDataBetweenComponent/src/app/child-component/child-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child-component',
+  templateUrl: './child-component.component.html',
+  styleUrls: ['./child-component.component.css']
+})
+export class ChildComponentComponent {
+  titles: string = 'Child Component';
+  messageChild: string = 'Message from Child Component';
+}
