3efa8267af66d7552514f41e84bc2a22c66cba4e
jireh-IT <kapangajireh@gmail.com>
2023-08-14 19:35:30 +0200

    share data from child component to parent component with @Viewchild

--- ShareDataBetweenComponent/src/app/app.component.ts
+++ ShareDataBetweenComponent/src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ChildComponentComponent } from './child-component/child-component.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'ShareDataBetweenComponent';
-  parentMessage = 'Parent Component Messagejjjjj';
+  parentMessage = 'Parent Component Message';
+  message: string | undefined;
+
+  @ViewChild(ChildComponentComponent) childComponent: any;
+  constructor() {
+    console.log(this.childComponent);
+  }
+  ngAfterViewInit(): void {
+    
+    this.message = this.childComponent.messageChild;
+  }
+
 }
